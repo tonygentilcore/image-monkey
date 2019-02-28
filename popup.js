ece57132de4dd2d1ee10b0d4bd066f6448558e1e
Tony Gentilcore <tonygentilcore@gmail.com>
2019-02-27 16:19:33 -0800

    First commit

---
+++ popup.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const filters = {
+  blur: 0,
+  brightness: 100,
+  contrast: 100,
+  grayscale: 0,
+  hue: 0,
+  invert: 0,
+  saturate: 100
+}
+
+const css = ({ blur, brightness, contrast, grayscale, hue, invert, saturate }) => `
+img {
+  filter: blur(${blur}px) brightness(${brightness}%) contrast(${contrast}%) grayscale(${grayscale}%) hue-rotate(${hue}deg) invert(${invert}%) saturate(${saturate}%) !important
+}
+`
+
+function applyFilter () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.insertCSS(tabs[0].id, { code: css(filters) })
+  })
+}
+
+const gui = new dat.GUI({ hideable: false })
+gui.add(filters, 'blur', 0, 10).onChange(applyFilter)
+gui.add(filters, 'brightness', 0, 400).onChange(applyFilter)
+gui.add(filters, 'contrast', 0, 400).onChange(applyFilter)
+gui.add(filters, 'grayscale', 0, 100).onChange(applyFilter)
+gui.add(filters, 'hue', 0, 360).onChange(applyFilter)
+gui.add(filters, 'invert', 0, 100).onChange(applyFilter)
+gui.add(filters, 'saturate', 0, 400).onChange(applyFilter)
